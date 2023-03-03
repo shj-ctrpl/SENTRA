9d61a60052e67d84d43c657bc11ac8ebfa2c4088
shj-ctrpl <creeptone@hanyang.ac.kr>
2023-03-03 17:08:26 +0900

    Now uses pair of universe id and cell id / lattice index

--- include/Collision.h
+++ include/Collision.h
@@ -1,21 +1,68 @@
 #pragma once
+
+#include <variant>
+
 #include "Geometries.h"
 
-class Collision {
-public:
-	std::vector<int> lattice_pos;
-	std::vector<int> univId;
-	std::vector<vector3<double>> biases;
-	vector3<double> bias;
+// int: Universe / lattice Id
+// std::variant<int, vector3<int>: cell Id / Lattice inner index
+typedef std::pair<int, std::variant<int, vector3<int>>> travel_info_t;
 
-	int lastuniv;
-	int surfId;
-	int cellId;
+class Collision {
+private:
+	// Travel histories: first = universe/lattice, second = fill/indices
+	std::vector<travel_info_t> travel_history;
+
+	// Biases, due to position change in the lattice
+	std::vector<vector3<double>> biases;
+	
+	int cellId;	// Cell Id where collision is happened
+	int surfId; // Surface Id where collision is happened
+
+public:
+	vector3<double> bias;
 
 	Collision();
 	~Collision() = default;
-
 	void Initialize();
-	void AddBias(const vector3<double>& bias);
-	void RemoveBias();
+	
+	//================================
+	// Setters
+	//================================
+	
+	// Set surface Id
+	void Set_Surf(int surfId) { this->surfId = surfId; }
+
+	// For universes: not adding any biases
+	void Add_travel(int univId, int fill);
+
+	// For lattices: adding indices and biases
+	void Add_travel(int univId, const vector3<int>& lattice_indice, const vector3<double>& lattice_bias);
+
+	// Pop last travel info
+	void Pop_travel();
+
+	
+	//================================
+	// Getters
+	//================================
+	
+	// Return the index of the collided surface
+	// If no surface found, int& surf will not change.
+	bool get_Surf(int& surf) const noexcept;
+	int get_Surf() const noexcept { return surfId; }
+
+	// Return the index of the last cell
+	// If no cell found, int& cell will not change.
+	bool get_Cell(int& cell) const noexcept;
+	int get_Cell() const noexcept { return cellId; }
+
+	// Return the index of the last universe
+	// If no universe found, int& univ will not change.
+	bool get_Univ(int& univ) const noexcept;
+	int get_Univ() const noexcept { return travel_history.back().first; }
+
+	std::vector<travel_info_t> Return_History() const noexcept {
+		return travel_history;
+	}
 };
--- src/Collision.cpp
+++ src/Collision.cpp
@@ -1,42 +1,71 @@
 #include <vector>
+#include <numeric>
 
 #include "Collision.h"
 #include "Geometries.h"
 
+using namespace std;
+
 Collision::Collision() {
-	univId = {};
+	travel_history = {};
 	biases = {};
 	bias = vector3<double>(0, 0, 0);
-	lastuniv = -1;
 	surfId = -1;
 	cellId = -1;
 }
 
 void Collision::Initialize() {
-	univId.clear();
+	travel_history.clear();
 	biases.clear();
 	bias = vector3<double>(0, 0, 0);
-	lastuniv = -1;
 	surfId = -1;
 	cellId = -1;
 }
 
-void Collision::AddBias(const vector3<double>& bias) {
-	biases.push_back(bias);
-	double x = 0, y = 0;
-	for (vector3<double>& v : biases) {
-		x += v.x;
-		y += v.y;
-	}
-	this->bias = vector3<double>(x, y, 0);
+void Collision::Add_travel(int univId, int fill) {
+	travel_history.push_back(pair(univId, fill));
 }
 
-void Collision::RemoveBias() {
-	if (!biases.empty()) { biases.pop_back(); }
-	double x = 0, y = 0;
-	for (vector3<double>& v : biases) {
-		x += v.x;
-		y += v.y;
+void Collision::Add_travel(int univId, const vector3<int>& lattice_index, const vector3<double>& lattice_bias) {
+	travel_history.push_back(pair(univId, lattice_index));
+	biases.push_back(lattice_bias);
+
+	bias = std::accumulate(biases.begin(), biases.end(), vector3<double>(0, 0, 0));
+}
+
+void Collision::Pop_travel() {
+	travel_history.pop_back();
+	// If the last travel info has vector3<int>, it means the last universe is a lattice
+	if (holds_alternative<vector3<int>> (travel_history.back().second)) {
+		biases.pop_back();
+		bias = std::accumulate(biases.begin(), biases.end(), vector3<double>(0, 0, 0));
 	}
-	this->bias = vector3<double>(x, y, 0);
+}
+
+bool Collision::get_Surf(int& surf) const noexcept {
+	if (surfId == -1) {
+		return false;
+	}
+	else {
+		surf = surfId;
+		return true;
+	}
+}
+
+bool Collision::get_Cell(int& cell) const noexcept {
+	if (cellId == -1) {
+		return false;
+	}
+	else {
+		cell = cellId;
+		return true;
+	}
+}
+
+bool Collision::get_Univ(int& univ) const noexcept {
+	if (!travel_history.empty()) {
+		univ = travel_history.back().first;
+		return true;
+	}
+	else return false;
 }
